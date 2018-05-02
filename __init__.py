71148c2e9d8da502efcd2aa13750ae6f7901de1d
peakMeissner <35743804+peakMeissner@users.noreply.github.com>
2018-05-02 14:35:43 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,6 +8,7 @@ from modules.core.props import Property
 from lib.gr8w8upd8m8.Processor import SingleEventProcessor
 from lib.gr8w8upd8m8.WiiBoard import ConstantReceiverBoard
 
+
 class WiiThread(threading.Thread):
     
     def __init__(self):
@@ -19,12 +20,12 @@ class WiiThread(threading.Thread):
         self.board = ConstantReceiverBoard(self.processor)
         
         if len(sys.argv) == 1:
-            print "Discovering board..."
+            cbpi.app.logger.info('Discover the Wii board')
             self.address = self.board.discover()
         else:
             self.address = sys.argv[1]
         
-        print "Trying to connect..."
+        cbpi.app.logger.info('Establish connection to the Wii board')
         self.board.connect(self.address)
         self.board.setLight(False)
     
@@ -33,13 +34,19 @@ class WiiThread(threading.Thread):
     
     def stop(self):
         self.running = False
-        print("Disconnect the WiiBoard")
+        cbpi.app.logger.info('Disconnect the Wii board')
         
         try:
+            
             # Disconnect already-connected devices.
             # This is basically Linux black magic just to get the thing to work.
-            subprocess.check_output(["bluez-test-input", "disconnect", self.address], stderr=subprocess.STDOUT)
-            subprocess.check_output(["bluez-test-input", "disconnect", self.address], stderr=subprocess.STDOUT)
+            
+            subprocess.check_output(['bluez-test-input', 'disconnect',
+                                    self.address],
+                                    stderr=subprocess.STDOUT)
+            subprocess.check_output(['bluez-test-input', 'disconnect',
+                                    self.address],
+                                    stderr=subprocess.STDOUT)
         except:
             pass
     
@@ -48,20 +55,19 @@ class WiiThread(threading.Thread):
             self.board.receive()
             self.value = self.processor.weight
             self.processor.done = False
-        
     
 
-@cbpi.sensor
 class WiiSensor(SensorPassive):
     
-    offset = Property.Number("Offset", True, 0)
-    unit_of_measure = Property.Select("Unit of measure", ["KG", "LBS"])
+    offset = Property.Number('Offset', True, 0)
+    unit_of_measure = Property.Select('Unit of measure', ['KG', 'LBS'])
     
     def init(self):
         self.t = WiiThread()
         
         def shudown():
             shudown.cb.shutdown()
+            
         shudown.cb = self.t
         
         self.t.start()
@@ -73,14 +79,16 @@ class WiiSensor(SensorPassive):
             pass
     
     def get_unit(self):
-        if self.unit_of_measure == "KG":
-            return "KG"
+        if self.unit_of_measure == 'KG':
+            return 'KG'
         else:
-            return "LBS"
+            return 'LBS'
     
     def read(self):
-        if self.unit_of_measure == "KG":
-            self.data_received(round(self.t.value + float(self.offset), 2))
+        if self.unit_of_measure == 'KG':
+            self.data_received(round(self.t.value + float(self.offset),
+                               2))
         else:
-            self.data_received(round((self.t.value * 2.20462262185) + float(self.offset), 2))
+            self.data_received(round(self.t.value * 2.20462262185
+                               + float(self.offset), 2))
     
